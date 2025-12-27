2621ce8a3239751de561b74f8e01d13c428cbe6d
CarlosLinux <cadudailha68@gmail.com>
2025-12-27 18:20:35 -0300

    feat: Criar entidade Venda e suas classes value-Objects
    Cria a classe Venda que representa a tabela venda do banco de dados
    além disso cria também suas classes de valores, que ainda estão vazias.

---
+++ src/main/java/model/Venda.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.Date;
+
+public class Venda {
+    private final Id id;
+    private final Date data;
+    private final Pagamento formaPagamento;
+    private final Vendedor nomeVendedor;
+    private final Quantidade quantidade;
+    private final Produto produto;
+    private final Total total;
+
+    public Venda(Id id, Date data, Pagamento formaPagamento, Vendedor nomeVendedor, Quantidade quantidade, Produto produto, Total total) {
+        this.id = id;
+        this.data = data;
+        this.formaPagamento = formaPagamento;
+        this.nomeVendedor = nomeVendedor;
+        this.quantidade = quantidade;
+        this.produto = produto;
+        this.total = total;
+    }
+}
